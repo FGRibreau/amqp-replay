b2adb845496ec2c9cb332e8c8ceb526ec51d84c4
FG Ribreau <github@fgribreau.com>
2016-05-10 15:13:16 +0200

    fix

--- amqp-replay.js
+++ amqp-replay.js
@@ -47,19 +47,3 @@ require('amqplib').connect(config.amqp.uri).then(function(conn) {
       exit();
     });
 }).then(null, console.warn);
-//
-// var argv = require('yargs').usage('Usage: $0 <command> [options]').option('J', {
-//   alias: 'jscs',
-//   default: './.jscsrc',
-//   description: 'jscsrc file path',
-//   type: 'string'
-// }).option('O', {
-//   alias: 'output',
-//   default: './README.html',
-//   description: 'HTML output file path',
-//   type: 'string'
-// }).help('h').alias('h', 'help').version().argv;
-//
-// var resolve = function resolve(path) {
-//   return p.resolve(process.cwd(), path);
-// };
